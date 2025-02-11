2bf465696de124e448718fe93aa1c5b1ab2d5787
SerGenesis <satanabakespancakes@mail.ru>
2025-02-12 01:37:06 +0300

    navmesh+enemy_follow

--- main.gd
+++ main.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
+@onready var target = $player
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+func _process(delta: float) -> void:
+	get_tree().call_group("enemy", "target_position", target.global_transform.origin)
--- presets/enemys/enemy_1/enemy_1.gd
+++ presets/enemys/enemy_1/enemy_1.gd
@@ -1,13 +1,20 @@
-extends Node3D
+extends CharacterBody3D
 class_name Enemy
 
-@onready var anim = $AnimationPlayer
+@onready var anim = $enemy_1/AnimationPlayer
+@onready var nav_agent :NavigationAgent3D= $NavigationAgent3D
+var gravity = 18
+var gravity_scale = 1
+
 
 enum STATE {
 	RUN,
 	ATTACK,
 	DIE
 }
+var speed = 5
+var player : CharacterBody3D
+var navigation : NavigationRegion3D
 
 var current_state = STATE.RUN
 # Called when the node enters the scene tree for the first time.
@@ -16,9 +23,16 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * gravity_scale * delta
 	if current_state == STATE.RUN:
 		look_at(Globals.player_pos)
+		var next_location = nav_agent.get_next_path_position()
+		var current_location = global_transform.origin
+		var new_velocity = (next_location - current_location).normalized() * speed
+		velocity = velocity.move_toward(new_velocity, 0.25)
+		move_and_slide()
 
 func die():
 	anim.play("dead")
@@ -27,8 +41,12 @@ func start():
 	anim.play("run")
 	anim.speed_scale = 3
 
+func target_position(target):
+	nav_agent.target_position = target
+
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if current_state != STATE.DIE:
+		body.queue_free()
 		die()
 		current_state = STATE.DIE
